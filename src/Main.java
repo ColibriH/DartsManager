b87407d044e3c470c80f6d365e66dfb887664530
vladislavs <vladprudnikov2@gmail.com>
2016-09-30 11:46:47 +0300

    * Start styling GameManagerGuiForm #0

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,16 @@ import MainController.MainController;
  * Created by vladislavs on 06.09.2016..
  */
 
-// TODO check memory leak
-// TODO Create documentation
 
 // TODO Finish Styling // Recreate group display for tournament
 
 // TODO Test for 3 people, 6, 9, 12
 
 // TODO more than 2 people in one group -> little bit changes in rules check this! (for this need huge refactor and logic changes) AND Create more game types
+
+// TODO In game menu
+
+// TODO Create documentation
 
 // Additional features
 // ==================================
@@ -22,10 +24,11 @@ import MainController.MainController;
 // TODO style configuration
 //===================================
 
-// TODO In game menu
-
+// TODO create inner resources project architecture
 // TODO Global Refactor: split logic
 // TODO Global End Refactor
+
+// TODO check memory leak
 
 public class Main
 {
