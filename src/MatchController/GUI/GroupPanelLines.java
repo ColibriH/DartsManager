22b05b8893678b6c20a0f234f8112f296aedd612
vladislavs <vladprudnikov2@gmail.com>
2016-11-01 11:12:14 +0200

    * Styling GeneratedGroupGuiForm #3

--- src/MatchController/GUI/GroupPanelLines.java
+++ src/MatchController/GUI/GroupPanelLines.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by Prud on 10/31/2016.
@@ -11,58 +13,144 @@ import java.util.HashMap;
 
 public class GroupPanelLines extends JPanel
 {
-    private HashMap <Integer, ArrayList<DisplayGroupPanel>> mGroupsPanels;
+	private HashMap <Integer, ArrayList <DisplayGroupPanel>> mGroupsPanels;
 
-    public GroupPanelLines(HashMap <Integer, ArrayList<DisplayGroupPanel>> groupsPanels)
-    {
-        if (groupsPanels != null)
-            mGroupsPanels = new HashMap <> (groupsPanels);
-    }
 
-    @Override
-    public Dimension getPreferredSize()
-    {
-        int size = 10 * 30;
-        return new Dimension(size, size);
-    }
+	public GroupPanelLines (HashMap <Integer, ArrayList <DisplayGroupPanel>> groupsPanels)
+	{
+		if (groupsPanels != null)
+			mGroupsPanels = new HashMap <> (groupsPanels);
+	}
 
-    @Override
-    public void paintComponent(Graphics g)
-    {
-        if (mGroupsPanels == null || mGroupsPanels.size() == 0)
-            return;
 
-        for (int k = 1; k < mGroupsPanels.size() + 1; k++)
-        {
-            if (k + 2 > mGroupsPanels.size())
-                break;
+	@Override
+	public Dimension getPreferredSize ()
+	{
+		int size = 10 * 30;
+		return new Dimension (size, size);
+	}
 
-            ArrayList<DisplayGroupPanel> a = mGroupsPanels.get(k);
-            ArrayList<DisplayGroupPanel> b = mGroupsPanels.get(k + 1);
 
-            int j = 0;
-            for (int i = 0; i < a.size(); i++)
-            {
-                if (i + 1 > a.size() - 1)
-                    break;
+	@Override
+	public void paintComponent (Graphics g)
+	{
+		if (mGroupsPanels == null || mGroupsPanels.size () == 0)
+			return;
 
-                Point a1p1 = new Point(a.get(i).getX() + a.get(i).getWidth(), a.get(i).getY() + (a.get(i).getHeight() / 2));
-                Point a1p2 = new Point(b.get(j).getX() + b.get(j).getWidth() / 2, a.get(i).getY() + (a.get(i).getHeight() / 2));
-                Point a1p3 = new Point(b.get(j).getX() + b.get(j).getWidth() / 2, b.get(j).getY());
+		HashMap <Integer, ArrayList <DisplayGroupPanel>> groupsPanels = cloneHashMap ();//new HashMap <> (mGroupsPanels);
 
-                Point a2p1 = new Point(a.get(i + 1).getX() + a.get(i + 1).getWidth(), a.get(i + 1).getY() + (a.get(i + 1).getHeight() / 2));
-                Point a2p2 = new Point(b.get(j).getX() + b.get(j).getWidth() / 2, a.get(i + 1).getY() + (a.get(i + 1).getHeight() / 2));
-                Point a2p3 = new Point(b.get(j).getX() + b.get(j).getWidth() / 2, b.get(j).getY() + b.get(j).getHeight());
+		boolean firstOdd = false;
 
-                g.drawLine(a1p1.x, a1p1.y, a1p2.x, a1p2.y);
-                g.drawLine(a1p2.x, a1p2.y, a1p3.x, a1p3.y);
+		for (int k = 1; k < groupsPanels.size () + 1; k++)
+		{
+			if (k + 1 > groupsPanels.size ())
+				break;
 
-                g.drawLine(a2p1.x, a2p1.y, a2p2.x, a2p2.y);
-                g.drawLine(a2p2.x, a2p2.y, a2p3.x, a2p3.y);
+			ArrayList <DisplayGroupPanel> a = groupsPanels.get (k);
+			ArrayList <DisplayGroupPanel> b = groupsPanels.get (k + 1);
 
-                j++;
-                i++;
-            }
-        }
-    }
+			int j = 0;
+			for (int i = 0; i < a.size (); i++)
+			{
+				ArrayList<DisplayGroupPanel> t = null;
+				DisplayGroupPanel curLvlPanel;
+				DisplayGroupPanel curLvlPanel2;
+				DisplayGroupPanel nextLvlPanel;
+
+				if (i + 1 > a.size () - 1)
+				{
+					if (firstOdd && k != groupsPanels.size ())
+					{
+						curLvlPanel     = a.get (i);
+						t = findPrev (groupsPanels);
+						curLvlPanel2    = t.get (0);
+
+						if (curLvlPanel2 == null)
+							break;
+
+						nextLvlPanel    = b.get (j);
+					}
+					else
+					{
+						firstOdd = true;
+						break;
+					}
+				}
+				else
+				{
+					curLvlPanel     = a.get (i);
+					curLvlPanel2    = a.get (i + 1);
+					nextLvlPanel    = b.get (j);
+				}
+
+				Point a1p1 = new Point (curLvlPanel.getX ()     + curLvlPanel.getWidth (),      curLvlPanel.getY ()  +  (curLvlPanel.getHeight () / 2));
+				Point a1p2 = new Point (nextLvlPanel.getX ()    + nextLvlPanel.getWidth () / 2, curLvlPanel.getY ()  +  (curLvlPanel.getHeight () / 2));
+				Point a1p3 = new Point (nextLvlPanel.getX ()    + nextLvlPanel.getWidth () / 2, nextLvlPanel.getY ());
+
+				Point a2p1 = new Point (curLvlPanel2.getX ()    + curLvlPanel2.getWidth (),     curLvlPanel2.getY () +  (curLvlPanel2.getHeight () / 2));
+				Point a2p2 = new Point (nextLvlPanel.getX ()    + nextLvlPanel.getWidth () / 2, curLvlPanel2.getY () +  (curLvlPanel2.getHeight () / 2));
+				Point a2p3 = new Point (nextLvlPanel.getX ()    + nextLvlPanel.getWidth () / 2, nextLvlPanel.getY () +   nextLvlPanel.getHeight ());
+
+				g.drawLine (a1p1.x, a1p1.y, a1p2.x, a1p2.y);
+				g.drawLine (a1p2.x, a1p2.y, a1p3.x, a1p3.y);
+
+				g.drawLine (a2p1.x, a2p1.y, a2p2.x, a2p2.y);
+				g.drawLine (a2p2.x, a2p2.y, a2p3.x, a2p3.y);
+
+				a.set (i, null);
+
+				j++;
+				i++;
+
+				if (i < a.size ())
+				{
+					a.set (i, null);
+				}
+				else
+				{
+					if (t != null)
+						t.remove (0);
+				}
+			}
+
+			Iterator <DisplayGroupPanel> i = a.iterator ();
+			while (i.hasNext ())
+			{
+				if (i.next () == null)
+					i.remove ();
+			}
+		}
+	}
+
+
+	private ArrayList <DisplayGroupPanel> findPrev (HashMap <Integer, ArrayList <DisplayGroupPanel>> groupsPanels)
+	{
+		Iterator it = groupsPanels.entrySet().iterator();
+		while (it.hasNext())
+		{
+			Map.Entry pair = (Map.Entry)it.next();
+			ArrayList <DisplayGroupPanel> a = (ArrayList <DisplayGroupPanel>)pair.getValue ();
+
+			if (a.size () != 0)
+				return a;
+		}
+
+		return null;
+	}
+
+
+	private HashMap <Integer, ArrayList <DisplayGroupPanel>> cloneHashMap ()
+	{
+		HashMap <Integer, ArrayList <DisplayGroupPanel>> tmp = new HashMap <> ();
+		Iterator it = mGroupsPanels.entrySet().iterator();
+		while (it.hasNext())
+		{
+			Map.Entry pair = (Map.Entry)it.next();
+			ArrayList <DisplayGroupPanel> a = new ArrayList <> ((ArrayList <DisplayGroupPanel>)pair.getValue ());
+
+			tmp.put (new Integer ((Integer)pair.getKey ()), a);
+		}
+
+		return tmp;
+	}
 }
--- src/MatchController/MatchController.java
+++ src/MatchController/MatchController.java
@@ -50,10 +50,12 @@ public class MatchController
 		mPlayerList.add(new PlayerObject("1", 1));
 		mPlayerList.add(new PlayerObject("2", 2));
 		mPlayerList.add(new PlayerObject("3", 3));
+
 		mPlayerList.add(new PlayerObject("4", 4));
 		mPlayerList.add(new PlayerObject("5", 5));
 		mPlayerList.add(new PlayerObject("6", 6));
 		mPlayerList.add(new PlayerObject("7", 7));
+
 		mPlayerList.add(new PlayerObject("8", 8));
 		mPlayerList.add(new PlayerObject("9", 9));
 
@@ -67,6 +69,10 @@ public class MatchController
 		mPlayerList.add(new PlayerObject("16", 16));
 		mPlayerList.add(new PlayerObject("17", 17));
 
+		mPlayerList.add(new PlayerObject("18", 18));
+		mPlayerList.add(new PlayerObject("19", 19));
+		mPlayerList.add(new PlayerObject("20", 20));
+		mPlayerList.add(new PlayerObject("21", 21));
 
 		whetherToKeepOldPlayerList ();
 		mGameManagerGuiForm = new GameManagerGuiForm (this);
